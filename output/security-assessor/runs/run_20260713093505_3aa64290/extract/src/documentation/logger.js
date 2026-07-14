f0af0212b0aa196b8bde743e5ed3f468f7980dcb
Mayank Jain <mayank@Mayanks-MacBook-Air.local>
2026-07-14 11:59:21 +0530

    fix file path issue

--- output/security-assessor/runs/run_20260713093505_3aa64290/extract/src/documentation/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = {
-  info: (...args) => console.log('[doc]', ...args),
-  debug: (...args) => console.debug('[doc]', ...args),
-  warn: (...args) => console.warn('[doc]', ...args),
-  error: (...args) => console.error('[doc]', ...args)
-};
-export default logger;
